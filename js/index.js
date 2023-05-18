7eb58961087e6693932a063b3e6078ff8743da6a
Edward <haredwcoder@gmail.com>
2023-05-18 17:05:38 -0500

    se_agrega_sub_galerias_js_pte_cerrar

--- js/index.js
+++ js/index.js
@@ -1,57 +1,62 @@
 
 /***SECCION PROYECTOS***/
 
+/***modal galeria1 */
 /*constante modal captura id-de contenedor general galeria html*/
 const modalApp = document.getElementById('modalApp');
-/*constante modal captura id contenedor img y boton html*/
-const modalBtn = document.getElementById('modalBtn');
-/*constante modal captura id img html (nota: boton sin id)*/
+
+/*constante modal captura id img html */
 const ventanaImg = document.getElementById('ventanaImg');
-/*constante captura id del boton cerrar modal inidividual*/
-const cerrar = document.getElementById('cerrar');
 
 /*escucar eventos con fx arrow que si es verdad que se da clic en una img con la clase 'subgaleria__modal-card' ocurre la funcion 'ver__img'*/
 modalApp.addEventListener('click', (m) => {
     m.stopPropagation();
     m.target.classList.contains('subgaleria__modal-card') ? ver_img(m) : '';
-    cerrar.classList.contains('hidden') ? cerrar__img() : '';
-
 })
 
-/*fx arrow muestra en console.log el contenido de la img al dar clic en alguna img de la galeria*/
+/*fx arrow muestra en consola el contenido de la img al dar clic en alguna img de la galeria*/
 const ver_img = (m) => {
     console.log(m.target);
     ventanaImg.src = m.target.src;
-    modalBtn.classList.toggle('hidden');
 }
 
-/*fx para boton cerrar de imagenes individuales de modal*/
-// const cerrar__img =() => {
-//     cerrar.remove('#modalBtn');
-// }
 
+/***modal galeria2 */
+/*constante modal captura id-de contenedor general galeria html*/
+const modalApp2 = document.getElementById('modalApp2');
 
+/*constante modal captura id img html */
+const ventanaImg2 = document.getElementById('ventanaImg2');
 
-
-
-
-
-/*prueba moda2*/
-// stopPropagation(); evita que se propaguen otros eventos sobre el objeto mi 
-
-document.querySelectorAll('.container__modal img').forEach(mod => {
-    mod.addEventListener('click', function (mi) {
-        mi.stopPropagation();
-        this.parentNode.classList.add('active');
-    })
+/*escucar eventos con fx arrow que si es verdad que se da clic en una img con la clase 'subgaleria__modal-card' ocurre la funcion 'ver__img'*/
+modalApp2.addEventListener('click', (m) => {
+    m.stopPropagation();
+    m.target.classList.contains('subgaleria__modal-card') ? ver_img2(m) : '';
 })
 
-document.querySelectorAll('.container__modal img').forEach(mod => {
-    mod.addEventListener('click', function (mi) {
-        this.classList.remove('active');
-        console.log('click');
-    })
+/*fx arrow muestra en consola el contenido de la img al dar clic en alguna img de la galeria*/
+const ver_img2 = (m) => {
+    console.log(m.target);
+    ventanaImg2.src = m.target.src;
+}
+
+
+/***modal galeria3 */
+/*constante modal captura id-de contenedor general galeria html*/
+const modalApp3 = document.getElementById('modalApp3');
+
+/*constante modal captura id img html */
+const ventanaImg3 = document.getElementById('ventanaImg3');
+
+/*escucar eventos con fx arrow que si es verdad que se da clic en una img con la clase 'subgaleria__modal-card' ocurre la funcion 'ver__img'*/
+modalApp3.addEventListener('click', (m) => {
+    m.stopPropagation();
+    m.target.classList.contains('subgaleria__modal-card') ? ver_img3(m) : '';
 })
 
-
+/*fx arrow muestra en consola el contenido de la img al dar clic en alguna img de la galeria*/
+const ver_img3 = (m) => {
+    console.log(m.target);
+    ventanaImg3.src = m.target.src;
+}
 
